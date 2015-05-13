45613b3d063e6758b53dae19cb5e97139d41ca7e
John Young & Jeanette Fairless <student@epicodus.com>
2015-05-13 16:07:06 -0700

    add the index, recipe erbs, and the app.rb.

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require("bundler/setup")
 Bundler.require(:default)
+require('pry')
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @recipes = Recipe.all()
+  erb(:index)
+end
+
+get('/recipe/:id') do
+  @recipe = Recipe.find(params.fetch('id'))
+  erb(:recipe)
+end
+
+
+post('/recipe_add/') do
+  @title = params.fetch('title')
+  @rating = params.fetch('rating')
+  Recipe.create({:title => @title, :rating => @rating})
+  @recipes = Recipe.all()
+  erb(:index)
+  # redirect('/')
+end
